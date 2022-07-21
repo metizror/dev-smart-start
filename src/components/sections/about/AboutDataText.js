ac241ca20dd73eb9e66c1c7ebb3f72025ffb4813
Abhinav <metizsoft@gmail.com>
2022-07-21 15:39:32 +0530

    change the color in about us file

--- src/components/sections/about/AboutDataText.js
+++ src/components/sections/about/AboutDataText.js
@@ -45,7 +45,7 @@ export class AboutDataText extends Component {
                     <div className="row">
                         <div className="col-md-6">
                             <div className="history-title mb-md-40 mt-5 ">
-                                <h2 className="text-light-black fs-2">Smart Start USA is the  <span className="text-light-green">Best Educational Brand!</span></h2>
+                                <h2 className="text-light-black fs-2">Smart Start USA is the  <span className="redColor">Best Educational Brand!</span></h2>
                                 <p className="text-light-white fs-5">Smart Start USA is an innovative after-school program designed for elementary, middle, and high school students. We offer interactive online courses that prepare students to excel in major academic areas and successfully conquer national academic contests, Regents Examinations, SAT, ISEE, and other essential assessments.</p>
                                 {/* <p className="text-light-white fs-5">
                                 Our classes are led by a team of highly qualified licensed teachers with an extensive academic background and mastery of virtual education. Smart Start USA inspires students to become future leaders, lifelong learners, and critical thinkers responsible for their own choices in life. Our curriculum is academically diverse, rigorous, and helps our students stay motivated while achieving their individual academic goals. In addition, we provide various educational resources such as testing materials, educational software, etc., to meet the unique needs of our learners. Therefore, all our students achieve remarkable test scores and shoot to the top of their classes.</p>
@@ -80,7 +80,7 @@ export class AboutDataText extends Component {
 
                     <p className="text-light-white fs-5">
                         Our classes are led by a team of highly qualified licensed teachers with an extensive academic background and mastery of virtual education. Smart Start USA inspires students to become future leaders, lifelong learners, and critical thinkers responsible for their own choices in life. Our curriculum is academically diverse, rigorous, and helps our students stay motivated while achieving their individual academic goals. In addition, we provide various educational resources such as testing materials, educational software, etc., to meet the unique needs of our learners. Therefore, all our students achieve remarkable test scores and shoot to the top of their classes.</p>
-                    <p className="text-light-green fs-5">Join us today and make Your Smart Start on the road to success!</p>
+                    <p className="redColor fs-5 ">Join us today and make Your Smart Start on the road to success!</p>
                 </Container>
             </section>
         )
--- src/components/sections/about/AboutVision.js
+++ src/components/sections/about/AboutVision.js
@@ -7,7 +7,7 @@ export class AboutVision extends Component {
             <div className="container">
                 <div className="row">
                     <div className='mt-3 col-md-6'>
-                        <h3> <span className="text-light-green fw-600">Our Mission </span></h3>
+                        <h3> <span className="redColor fw-600">Our Mission </span></h3>
                         <div className="history-title mb-md-40 text-light-white  fs-5">
                             Is to provide all students with various opportunities to develop their academic skills and conquer all obstacles they encounter on their academic roads. We inspire our students to become future leaders and make a difference in our rapidly evolving world.
                         </div>
@@ -21,7 +21,7 @@ export class AboutVision extends Component {
                         <img className='w-80' src="../../../assets/img/Aboutus/value.jpg" alt="" />
                     </div>
                     <div className='mt-3 col-md-6'>
-                        <h3> <span className="text-light-green  fw-600">Our Values</span></h3>
+                        <h3> <span className="redColor  fw-600">Our Values</span></h3>
                         <div className="history-title mb-md-40 text-light-white  fs-5">
                             Learning is the key to success. We strongly believe that by cultivating both cognitive and emotional intelligence, we can create the next generation of independent thinkers, lifelong learners, and responsible leaders.
                         </div>
@@ -31,7 +31,7 @@ export class AboutVision extends Component {
                 <div className="row">
 
                     <div className='mt-3 col-md-6'>
-                        <h3> <span className="text-light-green fw-600">
+                        <h3> <span className="redColor fw-600">
                             Our Vision</span></h3>
                         <div className="history-title mb-md-40 text-light-white  fs-5">
                             We envision a world where education is accessible to everyone. We are confident that all children can succeed academically if they get into the hands of professional and passionate teachers.
